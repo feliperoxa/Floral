7d29a4192c36adcec6ee34a8257deda7c5ec7947
feliperoxa <132163905+feliperoxa@users.noreply.github.com>
2024-05-20 19:54:53 -0300

    Add files via upload

---
+++ document_text.js
@@ -0,0 +1,22 @@
+//text section 'section_about'
+
+// class descrição_sobre
+
+document.getElementById("sobre_descricao_01").innerHTML="Aqui você encotra uma variedade de plantas e flores para decorar o seu ambiente interno e externo.";
+
+document.getElementById("sobre_descricao_02").innerHTML="Na nossa loja, não apenas cultivamos plantas, mas também cultivamos paixão pela natureza. Seja você um jardineiro experiente ou alguém que está apenas começando a descobrir o encanto das plantas, estamos aqui para oferecer um mundo de possibilidades botânicas.";
+
+document.getElementById("sobre_descricao_03").innerHTML="Além disso, comprometemo-nos com a qualidade e a sustentabilidade. Todas as nossas plantas são cultivadas com amor e responsabilidade, garantindo não apenas a beleza, mas também a saúde e vitalidade que você merece.";
+
+document.getElementById("texto_principal_01").innerHTML="Bem-vindo à Floral, onde a paixão pela natureza floresce em cada broto e cada pétala. Nós nos orgulhamos de ser mais do que uma simples loja de plantas - somos guardiões de um mundo verde, comprometidos em compartilhar o amor pelas maravilhas botânicas.";
+
+
+
+// class texto_principal principal_screen
+document.getElementById("texto_principal_02").innerHTML="O que nos diferencia é o nosso compromisso inabalável com a qualidade e a sustentabilidade. Cada planta que oferecemos é tratada com o carinho e a atenção que merece, desde o cultivo até o momento em que ela enfeita seu lar ou jardim. Nosso cuidado vai além do produto - estamos aqui para oferecer orientações, conselhos e suporte, garantindo que sua experiência com as plantas seja gratificante e bem-sucedida.";
+
+
+document.getElementById("texto_principal_03").innerHTML="Aqui na Floral, acreditamos no poder transformador da natureza. Queremos inspirar você a criar seus próprios espaços verdes, proporcionando não apenas beleza estética, mas também paz, harmonia e um vínculo renovado com o mundo natural ao seu redor.";
+
+
+document.getElementById("texto_principal_04").innerHTML="Junte-se a nós nessa jornada botânica. Explore, descubra e deixe-nos fazer parte da sua história com as plantas.";
